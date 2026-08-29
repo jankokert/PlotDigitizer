cadef0dc0ee6220e54e079cddaf7e4e7bb4df7c5
JanKokert <jan.kokert@gmail.com>
2026-08-29 09:16:57 +0200

    removed uv from startup

--- run test BAT46GW.cmd
+++ run test BAT46GW.cmd
@@ -1,2 +1,5 @@
-uv run plot_digitizer --image ".\tests\BAT46GW - Figure 3 - Typical Junction Capacitance.png" --grid --debug-svg -v
+@echo off
+REM No "uv" and no network access needed -- avoids the corporate self-signed-cert
+REM %~dp0 = folder this .cmd lives in.
+"%~dp0.venv\Scripts\plot_digitizer.exe" --image "%~dp0tests\BAT46GW - Figure 3 - Typical Junction Capacitance.png" --grid --method trace --debug-svg -v
 pause
